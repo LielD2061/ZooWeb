e8adccdf9e4b754c1316e6a20cd0f5d5c892b361
KrisiSaar <71778841+KrisiSaar@users.noreply.github.com>
2022-09-05 11:16:10 +0300

    Clone 5/9

--- ThirdWebZoo/Controllers/UserController.cs
+++ ThirdWebZoo/Controllers/UserController.cs
@@ -14,7 +14,7 @@ namespace ThirdWebZoo.Controllers
         }
         public IActionResult SignUp()
         {
-            return RedirectToAction("Index");
+            return View();/*RedirectToAction("Index")*/
         }
     }
 }
