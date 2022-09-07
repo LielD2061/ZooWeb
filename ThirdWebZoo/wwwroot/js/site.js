c8d2e9328c5e88f2e9da1f251cfce8c91e9af525
KrisiSaar <71778841+KrisiSaar@users.noreply.github.com>
2022-09-07 13:51:37 +0300

    Nimas li

--- ThirdWebZoo/wwwroot/js/site.js
+++ ThirdWebZoo/wwwroot/js/site.js
@@ -4,3 +4,10 @@ deleteMsg = "Your Delete succeeded"
 
 editMsg = "You Edit it Correctly"
 
+function ValidateSubmit() {
+    let test = document.getElementsByName("UserName");
+    let test2 = document.getElementsByName("Password");
+    if (test == null && test2 == null)
+        alert("Invalid input");
+    return "Correct";
+};
